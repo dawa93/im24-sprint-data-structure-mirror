81597776e9fa8f5e66afd583784f741199477959
Lee Hoyong <igotoweb@gmail.com>
2020-05-01 00:09:08 +0900

    add a comment for commonjs

--- part-2/src/hashTable.js
+++ part-2/src/hashTable.js
@@ -1,5 +1,7 @@
 const LimitedArray = require('./helpers/limitedArray');
 const hashFunction = require('./helpers/hashFunction');
+// 위 문법은 helpers 폴더에 있는 limitedArray와 hashFunction을 불러오는 문법입니다.
+// 위와 같이 require를 사용해서 다른 파일로부터 함수 등을 불러오는 작업은 이후에 따로 설명합니다.
 
 class HashTable {
   constructor() {
