58f1e1cc46853be870dd9b7b87186c1f71c3cd08
five1star <five1star0420@gmail.com>
2020-09-03 11:24:33 +0900

    queue done, but still have problem

--- part-1/src/queue.js
+++ part-1/src/queue.js
@@ -5,11 +5,33 @@ class Queue {
     this.rear = 0;
   }
 
-  size() {}
+  size() {
+    let sizeSum = this.rear - this.front;
+    return (sizeSum >= 0) ? sizeSum : 0;
+    // rear가 아니라, 객체의 pror의 갯수가 return 되어야한다.
+    // 객체의 prop의 갯수를 세는 methods는?
 
-  enqueue(element) {}
+  }
 
-  dequeue() {}
+  enqueue(element) {
+    this.rear++;
+    this.storage[this.rear] = element;
+    // rear + 1에 element가 추가되어야한다.
+
+  }
+
+  dequeue() {
+    this.front++;
+    let temp = this.storage[this.front];
+    // if (this.storage.hasOwnProperty('this.front')) {
+    delete this.storage[this.front];
+    return temp;
+
+
+    // front가 빠지고, front+1이 새로운 front가 된다.
+  }
 }
 
 module.exports = Queue;
+
+
