46b98139275a2116f54734b223b5d85436206c08
dawa93 <dawa93@naver.com>
2020-09-07 16:56:06 +0900

    pseudo code for binary

--- part-3/src/binarySearchTree.js
+++ part-3/src/binarySearchTree.js
@@ -4,12 +4,28 @@ class BinarySearchTreeNode {
     this.left = null;
     this.right = null;
   }
+  //        10 (root)
+  //     4           14
+  //  1   9      13      19
+  //            12      15
+  // 자식을 최대 2개 가질 수 있다.
+  // 왼쪽은 작은값 , 오른쪽이 같거나 큰값
+  insert(value) {
+    //child 트리는 트리 처럼 재귀
+    // let childTree = new BinarySearchTreeNode(value);
+    // value 가 this.value(=root value) 작으면 left
+    // value 가 this.value(=root value) 크면 right
+    if (value < this.value) {
+      this.left = value;
+    } else {
+      this.right = value;
+    }
+    //
+  }
 
-  insert(value) {}
+  // contains(value) {}
 
-  contains(value) {}
-
-  inorder(callback) {}
+  // inorder(callback) {}
 }
 
 module.exports = BinarySearchTreeNode;
